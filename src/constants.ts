0065d8be92ba9eb8c279c8c30da310e7b02609b2
Johnie Hjelm <johnie.hjelm@schibsted.com>
2025-12-15 12:44:02 +0100

    refactor: add CHARACTER_LIMIT constant and ResponseFormat enum to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,15 @@
 export const SKATTEVERKET_API_URL =
 	"https://skatteverket.entryscape.net/rowstore/dataset/70ccea31-b64c-4bf5-84c7-673f04f32505";
+
+/**
+ * Maximum response size in characters to prevent overwhelming responses
+ */
+export const CHARACTER_LIMIT = 25000;
+
+/**
+ * Response format options for tool outputs
+ */
+export enum ResponseFormat {
+	JSON = "json",
+	MARKDOWN = "markdown",
+}
